e9c5b9ee6c9321f31e578c0a2e66931e92e6b999
muratb1lg1n <68399694+muratb1lg1n@users.noreply.github.com>
2020-08-08 21:48:22 +0300

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+var kisa = document.getElementById("girdi").value
+function yaz(sayi){
+    document.getElementById("girdi").value = document.getElementById("girdi").value + sayi
+}
+function esit(){
+    document.getElementById("girdi").value = eval(document.getElementById("girdi").value)
+}
+function kare(){
+    document.getElementById("girdi").value = Math.pow(document.getElementById("girdi").value, 2)
+}
+function temiz(){
+    document.getElementById("girdi").value = ''
+}
+function sil(){
+    document.getElementById("girdi").value= document.getElementById("girdi").value.substring(0, document.getElementById("girdi").value.length-1)
+}
+function kok(){
+    document.getElementById("girdi").value = Math.sqrt(document.getElementById("girdi").value)
+}
